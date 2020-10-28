8babd76e392deed82e698ae05b61bbfb79cc5fe1
Opilane TTHK <opilane@tthk.ee>
2020-10-28 15:59:05 +0200

    first commit

---
+++ BirthDayofweek/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BirthDayofweek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DateAndTime/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DateAndTime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime today = DateTime.Today;
+            Console.WriteLine($"Today: {today}");
+            int yearToday = today.Year;
+            Console.WriteLine($"Current year from today {yearToday}");
+            var weekDayToday = today.DayOfWeek;
+            Console.WriteLine($"Its {weekDayToday}!");
+            Console.WriteLine("-----------------");
+
+            DateTime todayDetails = DateTime.Now;
+            Console.WriteLine($"Today in details: {todayDetails}");
+            DateTime utc = DateTime.UtcNow;
+            Console.WriteLine($"UTC Time {utc}");
+
+
+
+
+        }
+    }
+}
